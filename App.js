73676323012868fabd54a24c1737ed10b3ab870b
ArthurSkok <arthurskok00@gmail.com>
2022-03-29 11:48:03 -0400

    First

---
+++ App.js
@@ -0,0 +1,156 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {BleManager} from 'react-native-ble-plx';
+
+import requestLocationPermission from './permissions/requestPermissionLocation';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+};
+
+const App: () => Node = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+  const manager = new BleManager();
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+  const scanDevices = async () => {
+      const btState = await manager.state();
+      // test if bluetooth is powered on
+      if (btState !== 'PoweredOn') {
+        alert('Bluetooth is not powered on');
+        return false;
+      }
+      // explicitly ask for user's permission
+      const permission = await requestLocationPermission();
+      if (permission) {
+        manager.startDeviceScan(null, null, async (error, device) => {
+          console.log('Scanning ...');
+          // error handling
+          if (error) {
+            console.log(error);
+            return;
+          }
+          // found a bluetooth device
+          if (device.name) {
+            console.log(`${device.name} (${device.id})}`);
+          }
+
+          // stop scan after 5s
+          setTimeout(() => manager.stopDeviceScan(), 5000);
+        });
+      }
+      return true;
+    };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+    <TouchableOpacity style={styles.btnContainer} onPress={scanDevices}>
+            <Text>Start Scanning Device</Text>
+    </TouchableOpacity>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changess">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#000',
+    },
+    btnContainer: {
+      backgroundColor: '#fff',
+      padding: 5,
+    },
+});
+
+export default App;
