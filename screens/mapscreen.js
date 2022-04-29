675c783aeadc51eb6b27447556684c38ec393c58
ArthurSkok <arthurskok00@gmail.com>
2022-04-29 12:47:45 -0400

    Revert "Data now accessible from a new global store, used a relatively new library MMKV for this to allow for fast storage. Hope it isn't too buggy. GPS coordinate and marker now updates automatically, component seems to unmount correctly to stop updates so far."
    This reverts commit 95135da3

--- screens/mapscreen.js
+++ screens/mapscreen.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { StyleSheet, Text, View , Button, Dimensions } from 'react-native';
 import { Navigation } from 'react-native-navigation';
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
-import {storage} from './homescreen';
 const { width, height } = Dimensions.get('window');
 
 const ASPECT_RATIO = width / height;
@@ -11,26 +10,11 @@ const LONGITUDE = -70;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 class MapScreen extends React.Component {
-    constructor (props){
-              super(props)
-
-              this.state = {
-              lat:0 , lon:0, coordinates: [],
-          }
-    }
-    componentDidMount(){
-    this.interval = setInterval(() => this.setState({lat:storage.getString('latitude') , lon:storage.getString('longitude')}), 2000)
-    };
-    componentDidUnmount(){
-    clearInterval(this.interval);
-    };
 
   render() {
     return (
     <>
         <Text>This is the map screen</Text>
-        <Text>{this.state.lat}</Text>
-        <Text>{this.state.lon}</Text>
         <Button
                     title="Go to home screen"
                     onPress={() =>
@@ -49,11 +33,11 @@ class MapScreen extends React.Component {
              latitudeDelta: LATITUDE_DELTA,
              longitudeDelta: LONGITUDE_DELTA,
            }}
-           showUserLocation={true} >
+           /**showUserLocation={true} >
            <Marker coordinate={{
-             latitude:  parseFloat(this.state.lat),//parseFloat(this.state.lat, 10),
-             longitude:  parseFloat(this.state.lon), //parseFloat(this.state.lon, 10),
-           }} />
+             latitude: 37.78825,
+             longitude: -122.4324,
+           }}  **/>
        </MapView>
     </View>
     </>
